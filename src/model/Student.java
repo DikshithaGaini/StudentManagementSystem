8421f9c33cd244b27e0fb3f94a264225505879e7
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-08-09 19:54:21 +0530

    Implemented serializability

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,6 +1,8 @@
 package model;
 
-public class Student {
+import java.io.Serializable;
+
+public class Student implements Serializable {
 
     private int studentId;
     private String studentName;
